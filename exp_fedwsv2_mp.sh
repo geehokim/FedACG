af2d44c520d53184eb5eac847676ce9bd2b5a19f
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-07-11 14:29:44 +0900

    Updates

--- exp_fedwsv2_mp.sh
+++ exp_fedwsv2_mp.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0,1 \
-python federated_train.py multiprocessing=True main_gpu=0 client=base server=FedWS exp_name=FedWS dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 batch_size=50 wandb=True trainer.local_lr_decay=0.995
+python federated_train.py multiprocessing=True main_gpu=0 client=base server=FedWSV2 exp_name=FedWSV2 dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 batch_size=50 wandb=True defaults.model=resnet18_GN
