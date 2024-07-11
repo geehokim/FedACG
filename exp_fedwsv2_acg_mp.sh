7331f645f13881dbaccb63927ce93c2877ffd2b2
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-07-11 19:22:02 +0900

    Updates

--- exp_fedwsv2_acg_mp.sh
+++ exp_fedwsv2_acg_mp.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0,1 \
-python federated_train.py multiprocessing=True main_gpu=0 client=ACG server=FedACG exp_name=FedWSV2_ACG dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 wandb=True model=resnet18_WS trainer.local_lr_decay=0.995
+python federated_train.py multiprocessing=True main_gpu=0 client=base server=FedACG exp_name=FedWSV2_baseACG dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 wandb=True model=resnet18_WS trainer.local_lr_decay=0.995
