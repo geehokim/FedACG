97bf0836a9a5c18390381f0f941c3a4f22735b35
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-07-22 13:54:26 +0900

    Updates

--- exp_feddecorr.sh
+++ exp_feddecorr.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0 \
-python federated_train.py client=Decorr server=base exp_name=FedDyn dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 batch_size=50 wandb=True
+python federated_train.py client=Decorr server=base exp_name=FedDecorr dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 batch_size=50 wandb=True
