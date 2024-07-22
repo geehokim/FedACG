ef69e00482d3dec2f693cbb255a8120ae0e03d92
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-07-22 14:00:56 +0900

    Updates

--- exp_feddyn_500_2.sh
+++ exp_feddyn_500_2.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0 \
-python federated_train.py client=Dyn server=FedDyn exp_name=FedDyn dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 wandb=True
+python federated_train.py client=Dyn server=FedDyn exp_name=FedDyn_500_2 dataset=cifar100 trainer.num_clients=500 split.alpha=0.3 trainer.participation_rate=0.02 batch_size=10 wandb=True
