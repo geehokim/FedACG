31b07af205e586d8a26d3aa95613c3df95da7173
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-08-14 13:16:21 +0900

    Upates

---
+++ exp_fedwsv2_dyn_500_2.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 \
+python federated_train.py client=Dyn server=FedDyn exp_name=FedWSV2_Dyn_500_2 dataset=cifar100 trainer.num_clients=500 split.alpha=0.3 trainer.participation_rate=0.02 batch_size=10 wandb=True model=resnet18_WS 
