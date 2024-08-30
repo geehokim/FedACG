9f4aecb8647e254a434f593421288990a25fe39e
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-08-30 14:18:12 +0900

    Updates

--- models/resnet_ws.py
+++ models/resnet_ws.py
@@ -22,6 +22,7 @@ class WSConv2d(nn.Conv2d):
                  padding=0, dilation=1, groups=1, bias=True):
         super(WSConv2d, self).__init__(in_channels, out_channels, kernel_size, stride,
                  padding, dilation, groups, bias)
+        self.rho=1e-4
 
     def forward(self, x):
         weight = self.weight
@@ -29,9 +30,11 @@ class WSConv2d(nn.Conv2d):
                                   keepdim=True).mean(dim=3, keepdim=True)
         weight = weight - weight_mean
         std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
-        weight = weight / std.expand_as(weight)
+        weight = weight / std.expand_as(weight) * self.rho
+
         return F.conv2d(x, weight, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
+
 
 class BasicBlockWS(nn.Module):
     expansion = 1
