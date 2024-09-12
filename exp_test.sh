877819a8a355e05dcbccbddcf13fb7b723929925
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-09-12 17:38:49 +0900

    Updates

--- exp_test.sh
+++ exp_test.sh
@@ -4,9 +4,9 @@ BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-LR_DECAY=0.998
+LR_DECAY=0.995
 
-python3 federated_train.py multiprocessing=True main_gpu=0 server=FedACG client=ACG exp_name=FedACGGN_rho1e-1e-4 dataset=${DATASET} project=test \
+python3 federated_train.py multiprocessing=True main_gpu=0 server=FedACG client=base exp_name=FedAVGGNbase_rho1 dataset=${DATASET} project=test \
 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 \
 batch_size=${BATCH_SIZE} wandb=True trainer.local_lr_decay=${LR_DECAY} model=resnet18_WS \
 trainer.local_epochs=5 trainer.global_rounds=1000
