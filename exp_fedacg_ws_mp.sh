d1fe9da6ed67729892be63325a6500473aab341e
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-09-12 17:57:37 +0900

    Updates

--- exp_fedacg_ws_mp.sh
+++ exp_fedacg_ws_mp.sh
@@ -6,4 +6,4 @@ if [ ${DATASET} = "tinyimagenet" ];then
 fi
 LR_DECAY=0.998
 
-python federated_train.py multiprocessing=True main_gpu=0 server=FedACG client=ACG exp_name=FedACGWS dataset=${DATASET} trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 batch_size=${BATCH_SIZE} wandb=True trainer.local_lr_decay=${LR_DECAY} model=resnet18_WS
+python federated_train.py multiprocessing=True main_gpu=0 server=FedACG client=ACG exp_name=FedACGWS dataset=${DATASET} trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 batch_size=${BATCH_SIZE} wandb=False trainer.local_lr_decay=${LR_DECAY} model=resnet18_WS
