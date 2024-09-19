3e3e414a37cea76bab2ca018f90ae0dbfe2d6031
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-09-19 14:11:50 +0900

    temp

--- exp_fedavg_mp.sh
+++ exp_fedavg_mp.sh
@@ -1,10 +1,12 @@
 CUDA_VISIBLE_DEVICES=0,1
-DATASET=cifar100
+DATASET=tinyimagenet
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
+ALPHA=0.1
 
-python federated_train.py multiprocessing=True main_gpu=0 client=base server=base exp_name=FedAvgIID \
-dataset=${DATASET} trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 \
-batch_size=${BATCH_SIZE} wandb=True split.mode=iid
+python federated_train.py multiprocessing=True main_gpu=0 client=base server=base exp_name=FedAvg_"$ALPHA" \
+dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
+batch_size=${BATCH_SIZE} wandb=True
+#  split.mode=iid
--- exp_fedavgm.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=0, \
-python federated_train.py client=base server=FedAvgM exp_name=FedAvgM dataset=cifar100 trainer.num_clients=100 split.alpha=0.3 trainer.participation_rate=0.05 batch_size=50 wandb=True
