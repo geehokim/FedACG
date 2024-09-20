cdf369d1f52631f7f23d15f1659f638145fa02e1
gymlab <swkim@pknu.ac.kr>
2024-09-20 19:01:13 +0900

    Updates

--- exp_fedavgm.sh
+++ exp_fedavgm.sh
@@ -1,12 +1,12 @@
 CUDA_VISIBLE_DEVICES=0
-DATASET=cifar10
+DATASET=cifar100
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-ALPHA=0.05
+ALPHA=0.3
 
 python3 federated_train.py client=base server=FedAvgM exp_name=FedAvgM_"$ALPHA" \
-dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
-batch_size=${BATCH_SIZE} wandb=True \
+dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.02 \
+batch_size=${BATCH_SIZE} wandb=True project="FedWS_2_100" \
 # split.mode=iid
