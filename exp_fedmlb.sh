f8c57c624539b546dca017a079cde448e4c79741
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-09-20 14:40:17 +0900

    Updates

--- exp_fedmlb.sh
+++ exp_fedmlb.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1
+CUDA_VISIBLE_DEVICES=0
 DATASET=tinyimagenet
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
@@ -6,7 +6,7 @@ if [ ${DATASET} = "tinyimagenet" ];then
 fi 
 ALPHA=0.05
 
-python3 federated_train.py multiprocessing=True main_gpu=0 client=MLB server=FedAvgM exp_name=FedMLB_"$ALPHA" \
+python3 federated_train.py client=MLB server=FedAvgM exp_name=FedMLB_"$ALPHA" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
 batch_size=${BATCH_SIZE} wandb=True model=resnet18_MLB \
 # split.mode=iidd
