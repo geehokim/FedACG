061e2300d36484bb5410f2bb9d0d23e139b087e3
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-10-24 21:26:38 +0900

    updates

--- exp_fedavgws_drift.sh
+++ exp_fedavgws_drift.sh
@@ -6,7 +6,7 @@ if [ ${DATASET} = "tinyimagenet" ];then
 fi 
 ALPHA=0.3
 
-python3 federated_train.py client=DriftBase server=base trainer=drift exp_name=FedAvg_"$ALPHA" \
+python3 federated_train.py client=DriftBase server=base trainer=drift exp_name=FedAvgWS_"$ALPHA" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
 batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="client_drift" \
 # split.mode=iid
