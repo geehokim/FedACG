77423b0b0c8ac1b4aa8a55571ee529c509c221c1
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-12-12 19:13:57 +0900

    updates

--- federated_train.py
+++ federated_train.py
@@ -1,15 +1,14 @@
 import os
 from pathlib import Path
 
-os.environ['CUDA_VISIBLE_DEVICES'] = "0,1"
+# os.environ['CUDA_VISIBLE_DEVICES'] = "0,1"
 
 import torch
 import wandb
 from torch.multiprocessing import set_start_method
-from torch.utils.data import DataLoader
-from datasets.build import build_dataset, build_datasets
+from datasets.build import build_datasets
 from models.build import build_encoder
-from servers.build import get_server_type, build_server
+from servers.build import build_server
 from clients.build import get_client_type
 from evalers.build import get_evaler_type
 from trainers.build import get_trainer_type
@@ -19,20 +18,18 @@ from utils import initalize_random_seed
 import hydra
 from omegaconf import DictConfig
 import omegaconf
-import coloredlogs, logging
-# import loggings
+import logging
 logger = logging.getLogger(__name__)
-# coloredlogs.install(fmt='%(asctime)s %(name)s[%(process)d] %(levelname)s %(message)s')
-coloredlogs.install(level='INFO', fmt='%(asctime)s %(name)s[%(process)d] %(message)s', datefmt='%m-%d %H:%M:%S')
 
 wandb.require("service")
 
-@hydra.main(version_base=None, config_path="configs", config_name="config")
+@hydra.main(version_base=None, config_path="configs", config_name="config") # load configs/config.yaml as args
 def main(args : DictConfig) -> None:
 
-    torch.multiprocessing.set_sharing_strategy('file_system')
+    torch.multiprocessing.set_sharing_strategy('file_system')   # the way of shared files in multi-processing
     set_start_method('spawn', True)
     # pid = os.getpid()
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.visible_devices
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 
     args.log_dir = Path(args.log_dir)
@@ -57,6 +54,7 @@ def main(args : DictConfig) -> None:
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("device", device)
 
+    # initialize moddules
     model = build_encoder(args)
     client_type = get_client_type(args)
     server = build_server(args)
