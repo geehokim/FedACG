14fe302359316998b6f696c1c0c164356f6e6e31
성열 <qksksktjdduf@naver.com>
2024-12-27 16:13:41 +0900

    AQD config

--- utils/qunat_function.py
+++ utils/qunat_function.py
@@ -8,9 +8,11 @@ def AQD_update(model, args):
             if 'first-last' in args.model.keyword and name == 'conv1.weight':
                 first_quant_conv = quant_conv(param.shape[0], param.shape[1], kernel_size=param.shape[2], args=args)
                 param.data.copy_(first_quant_conv(param.data))
+                
             elif name != "conv1.weight" and ("conv1.weight" in name or "conv2.weight" in name):
                 layer_quant_conv = quant_conv(param.shape[0], param.shape[1], kernel_size=param.shape[2], args=args)
                 param.data.copy_(layer_quant_conv(param.data))
+                
             elif "downsample.0.weight" in name:
                 quant_conv1x1 = nn.Sequential(
                     quant_conv(param.shape[0], param.shape[1], kernel_size=1, args=args),
