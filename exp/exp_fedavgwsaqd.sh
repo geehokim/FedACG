a2ab01ca441c75e73c9b04968c874101ed7a103c
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-12-28 23:30:47 +0900

    updates

---
+++ exp/exp_fedavgwsaqd.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0
+DATASET=cifar100
+BATCH_SIZE=50
+if [ ${DATASET} = "tinyimagenet" ];then
+    BATCH_SIZE=100
+fi 
+ALPHA=0.3
+NBITS=1
+
+python3 federated_train.py client=base server=base visible_devices=\'0\' exp_name=FedAvgWSAQD_"$ALPHA"_"B$NBITS" \
+dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
+quantizer=AQD quantizer.wt_bit=${NBITS} \
+batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="FedWS_5_100" \
+# split.mode=iid
