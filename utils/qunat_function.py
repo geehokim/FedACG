f0141d3a61c7638f578e63c786602f2aab54e9be
Seung-Wook Kim <swkim@pknu.ac.kr>
2024-12-30 15:42:17 +0900

    update

--- utils/qunat_function.py
+++ utils/qunat_function.py
@@ -19,20 +19,17 @@ def AQD_update(model, args):
                 param.data.copy_(last_quant_linear(param.data))
 
 
-class WSQConv2d(nn.Conv2d):
+class WSQConv2d(nn.Module):
     bit1 = [0.7979]
     bit2 = [0.5288, 0.9816]
     bit3 = [0.4510, 0.7481, 0.9882]
     bit4 = [0.2960, 0.5567, 0.7088, 1.1286]
     bit8 = [0.05, 0.1, 0.2, 0.3375, 0.5250, 0.9875, 1.3875, 1.45]
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True, rho=1e-3, n_bits=1, clip_prob=-1):
-        super(WSQConv2d, self).__init__(in_channels, out_channels, kernel_size, stride,
-                 padding, dilation, groups, bias)
+    def __init__(self, n_bits=1, clip_prob=-1):
+        super(WSQConv2d, self).__init__()
         
         self.alpha = torch.tensor(getattr(self, f'bit{n_bits}'), dtype=torch.float32)
-        self.rho = rho
         self.clip_prob = clip_prob
         
         # Generate all combinations of b_k in {-1, 1} for 2^(M-1) terms
@@ -82,21 +79,14 @@ def WSQ_update(model, args):
     for name, param in model.named_parameters():
         if hasattr(args.quantizer, 'keyword'):
             if 'first-last' in args.quantizer.keyword and name == 'conv1.weight':
-                first_quant_conv = WSQConv2d(param.shape[1], param.shape[0], kernel_size=param.shape[2], 
-                                             n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
+                first_quant_conv = WSQConv2d(n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
                 param.data.copy_(first_quant_conv(param.data)) 
             elif name != "conv1.weight" and ("conv1.weight" in name or "conv2.weight" in name):
-                layer_quant_conv = WSQConv2d(param.shape[1], param.shape[0], kernel_size=param.shape[2], 
-                                             n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
+                layer_quant_conv = WSQConv2d(n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
                 param.data.copy_(layer_quant_conv(param.data))
             elif "downsample.0.weight" in name:
-                quant_conv1x1 = WSQConv2d(param.shape[1], param.shape[0], kernel_size=1, 
-                                          n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
+                quant_conv1x1 = WSQConv2d(n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
                 param.data.copy_(quant_conv1x1(param.data))
-            elif 'first-last' in args.quantizer.keyword and name == 'fc.weight':
-                last_quant_linear = quant_linear(args.model.last_feature_dim, args.num_classes, bias=True, 
-                                                 n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
-                param.data.copy_(last_quant_linear(param.data))
 
                 
 class WLQConv2d(nn.Conv2d):
